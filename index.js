350ae35667f248a53537eab06d82511dfc7d77b1
Eren S <esarac@outlook.com.tr>
2024-04-10 07:39:18 +0000

    add /api/whoami handler

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress:
+      (req.headers["x-forwarded-for"] || "").split(",")[0] ||
+      req.connection.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
+})
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
